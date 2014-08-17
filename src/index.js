77bcedf6a5b250d2ed89ed5de1cab188397b537a
david.bashford <dbashford@bericotechnologies.com>
2014-08-17 17:10:10 -0400

    scaffold

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
+"use strict";
+
+var acorn
+  , amdTraverse
+  , commonTraverse
+  ;
+
+// Execute acorn parse
+// If parse fails, return undefined ast
+var _parseText = function( text ) {
+  // lazy load
+  if ( !acorn ) {
+    acorn = require( "acorn" );
+  }
+
+  var ast;
+  try {
+    ast = acorn.parse( text );
+  } catch ( err ) {
+    // just going to let ast remain undefined
+  }
+  return ast;
+};
+
+var _determineIfAMD = function( ast ) {
+  return true;
+};
+
 module.exports = function( text ) {
-  return [];
-}
+  var ast = _parseText( text );
+  if ( !ast ) {
+    return [];
+  }
+
+  var isAMD = _determineIfAMD( ast );
+
+  var names = [];
+  if ( isAMD ) {
+    // lazy load
+    if ( !amdTraverse ) {
+      amdTraverse = require( "./amd" );
+    }
+    names = amdTraverse.names( ast );
+  } else {
+    // lazy load
+    if ( !commonTraverse ) {
+      commonTraverse = require( "./commonjs" );
+    }
+    names = commonTraverse.names( ast );
+  }
+
+  return names;
+};
